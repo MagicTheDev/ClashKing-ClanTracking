d3f8b522c7ffcd34e368308f73bcfd068d752c61
Magic <82341152+MagicTheDev@users.noreply.github.com>
2023-03-08 22:45:44 -0600

    initial

--- src/events/clan_events.py
+++ src/events/clan_events.py
@@ -44,8 +44,8 @@ async def member_leave(member: coc.ClanMember, new_clan: coc.Clan):
         clan_tasks.append(task)
     await asyncio.gather(*clan_tasks, return_exceptions=False)
 
-@coc.ClanEvents.member_donations()
-async def member_donos(old_member: coc.ClanMember, new_member: coc.ClanMember):
+@coc.ClanEvents.bulk_donations()
+async def member_donos(old_clan: coc.Clan, clan: coc.Clan):
     clan_tasks = []
     async def send_ws(ws, json):
         try:
@@ -56,28 +56,7 @@ async def member_donos(old_member: coc.ClanMember, new_member: coc.ClanMember):
             except:
                 pass
 
-    json_data = {"type": "member_donations", "old_member": old_member._raw_data,
-                 "new_member": new_member._raw_data}
-
-    for client in settings.CLAN_CLIENTS.copy():
-        task = asyncio.ensure_future(send_ws(ws=client, json=json_data))
-        clan_tasks.append(task)
-    await asyncio.gather(*clan_tasks, return_exceptions=False)
-
-@coc.ClanEvents.member_received()
-async def member_received(old_member: coc.ClanMember, new_member: coc.ClanMember):
-    clan_tasks = []
-    async def send_ws(ws, json):
-        try:
-            await ws.send_json(json)
-        except:
-            try:
-                settings.CLAN_CLIENTS.remove(ws)
-            except:
-                pass
-
-    json_data = {"type": "member_received", "old_member": old_member._raw_data,
-                 "new_member": new_member._raw_data}
+    json_data = {"type": "all_member_donations", "old_clan" : old_clan._raw_data, "new_clan" : clan._raw_data}
 
     for client in settings.CLAN_CLIENTS.copy():
         task = asyncio.ensure_future(send_ws(ws=client, json=json_data))
