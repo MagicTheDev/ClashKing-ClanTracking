7cefe1c5dd1d6ffae24f9a2106c28ace571aba64
Magic <82341152+MagicTheDev@users.noreply.github.com>
2023-03-09 20:34:41 -0600

    initial

--- src/events/war_events.py
+++ src/events/war_events.py
@@ -16,7 +16,12 @@ async def new_war(new_war: coc.ClanWar):
             except:
                 pass
 
-    json_data = {"type": "new_war", "war": new_war._raw_data}
+    league_group = None
+    if new_war.is_cwl:
+        league_group: coc.ClanWarLeagueGroup = new_war.league_group
+        league_group = league_group._raw_data
+
+    json_data = {"type": "new_war", "war": new_war._raw_data, "league_group" : league_group}
 
     for client in settings.WAR_CLIENTS.copy():
         task = asyncio.ensure_future(send_ws(ws=client, json=json_data))
